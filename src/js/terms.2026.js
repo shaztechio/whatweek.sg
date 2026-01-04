a1405c2568a17d67b561171b83961e07d009c31a
Shazron Abdullah <36107+shazron@users.noreply.github.com>
2026-01-04 14:36:19 +0800

    Add 2026 and 2027 terms files

---
+++ src/js/terms.2026.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    term: 1,
+    start: 1,
+    end: 10,
+    break: [11],
+  },
+  {
+    term: 2,
+    start: 12,
+    end: 21,
+    break: [22, 23, 24, 25],
+  },
+  {
+    term: 3,
+    start: 26,
+    end: 35,
+    break: [36],
+  },
+  {
+    term: 4,
+    start: 37,
+    end: 46,
+    break: [47, 48, 49, 50, 51, 52, 53],
+  },
+];
